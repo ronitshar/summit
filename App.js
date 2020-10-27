7e9cce05a33b7963147434cf4f87df80db997f28
mocha123 <sharmaronit009@gmail.com>
2020-10-26 22:48:54 -0700

    basic login page, no submit button

--- App.js
+++ App.js
@@ -1,11 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { TextInput, StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.title}>Summit</Text>
+      <Text style={styles.subtitle}>Login with Zoom</Text>
+      <TextInput style={{ height: 40, borderColor: 'gray', borderWidth: 1, width: 300, margin: 50}}
+                 placeholder="  Username" autofocus={true}
+      />
+      <TextInput style={{ height: 40, borderColor: 'gray', borderWidth: 1, width: 300 }}
+                 placeholder="  Password" secureTextEntry={true}
+      />
       <StatusBar style="auto" />
     </View>
   );
@@ -13,9 +20,17 @@ export default function App() {
 
 const styles = StyleSheet.create({
   container: {
+    margin: 100,
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    fontSize: 30
   },
+  title: {
+    fontSize: 30
+  },
+  subtitle: {
+    fontSize: 20,
+    marginTop: 30
+  }
 });
